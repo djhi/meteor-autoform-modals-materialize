9513e22e89fe2954c26a86d77070ac6d5558eb55
Alexander Ottenhoff <alexander.ottenhoff@gmail.com>
2016-03-08 14:37:56 +0100

    Bumped Package Version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'gildaspk:autoform-modals-materialize',
     summary: "Create, update and delete collections with materialize modals",
-    version: "0.0.8",
+    version: "0.0.9",
     git: "https://github.com/djhi/meteor-autoform-modals-materialize",
 });
 
